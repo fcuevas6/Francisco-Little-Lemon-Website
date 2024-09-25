75469e29c0f6225bfdbde41906a5c9818191217d
Francisco Cuevas <124302532+fcuevas6@users.noreply.github.com>
2024-09-25 14:18:40 -0400

    Footer Layout
    Added basic layout for the footer

--- little-lemon/src/components/layout/Footer.js
+++ little-lemon/src/components/layout/Footer.js
@@ -1,6 +1,50 @@
+import "./Footer.css"
+import whiteLogo from "./assets/logo-white.png"
 export default function Footer() {
+
+    const contactInfo = [
+        {icon: "", info: "143 Main St. Chicago, IL 60610"},
+        {icon: "", info: "(312)555-1234"},
+        {icon: "", info: "info@littlelemon.com"}
+    ]
     return (
-       // <h1>Footer Here</h1>
-       <></>
+       <footer className="footer-container">
+            <div>
+                <img src={whiteLogo} alt="Little Lemon logo" className="footer-logo" />
+            </div>
+            <div>
+                <nav>
+                    <h4>Site Map</h4>
+                    <ul>
+                        <li>Home</li>
+                        <li>Menu</li>
+                        <li>About us</li>
+                        <li>Reservations</li>
+                        <li>Order Online</li>
+                    </ul>
+                </nav>
+            </div>
+            <div>
+                <nav>
+                    <h4>Contact us</h4>
+                    <ul>
+                        <li>Address</li>
+                        <li>Phone</li>
+                        <li>Email</li>
+                    </ul>
+                </nav>
+            </div>
+            <div>
+                <nav>
+                    <h4>Connect with us</h4>
+                    <ul>
+                        <li>Instagram</li>
+                        <li>Facebook</li>
+                        <li>Yelp</li>
+                        <li>TikTok</li>
+                    </ul>
+                </nav>
+            </div>
+       </footer>
     )
 }
